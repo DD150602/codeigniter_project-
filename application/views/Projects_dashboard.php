57f5d79e45b94ff048d986d934093d4e5e05dc36
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-05 14:48:45 -0500

    feat(Views): :sparkles: Add modal for updatind
    A modal is added to update the user information

--- application/views/Projects_dashboard.php
+++ application/views/Projects_dashboard.php
@@ -31,9 +31,8 @@
           <img src="https://via.placeholder.com/40" alt="" width="32" height="32" class="rounded-circle me-2">
           <strong>User</strong>
         </a>
-        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser1">
-          <li><a class="dropdown-item" href="#">Settings</a></li>
-          <li><a class="dropdown-item" href="#">Profile</a></li>
+        <ul class="dropdown-menu text-small shadow dropdown-menu-end" aria-labelledby="dropdownUser1">
+          <li><button type="button" class="dropdown-item btn" mary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Profile Settings</button></li>
           <li>
             <hr class="dropdown-divider">
           </li>
@@ -116,6 +115,67 @@
       </article>
     </section>
   </main>
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div class="w-100 d-flex justify-content-center position-relative">
+            <h1 class="modal-title fs-5 text-center" id="staticBackdropLabel">Update User Info</h1>
+          </div>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body d-flex flex-column">
+          <div>
+            <form id="user_info_update" action="">
+              <div class="row">
+                <div class="mb-3 col-6">
+                  <label for="user_name" class="form-label">Name</label>
+                  <input type="password" class="form-control" id="user_name" placeholder="Name">
+                </div>
+                <div class="mb-3 col-6">
+                  <label for="user_lastname" class="form-label">Lastname</label>
+                  <input type="password" class="form-control" id="user_lastname" placeholder="Lastname">
+                </div>
+                <div class="mb-3 col-6">
+                  <label for="user_username" class="form-label">username</label>
+                  <input type="password" class="form-control" id="user_username" placeholder="Username">
+                </div>
+                <div class="mb-3 col-6">
+                  <label for="user_email" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="user_email" placeholder="name@example.com">
+                </div>
+                <div class="mb-3 d-flex justify-content-center">
+                  <input class="btn btn-primary" type="submit" value="Update information">
+                </div>
+            </form>
+          </div>
+          <form id="user_password_change">
+            <div class="row">
+              <div class="mb-3 col-6">
+                <label for="Old_password" class="form-label">Old password</label>
+                <input type="email" class="form-control" id="Old_password" placeholder="Your old Password">
+              </div>
+              <div class="mb-3 col-6">
+                <label for="user_password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="user_password" placeholder="New Password">
+              </div>
+              <div class="mb-3 col-6">
+                <label for="user_password_confirm" class="form-label">Confirm your Password</label>
+                <input type="password" class="form-control" id="user_password_confirm" placeholder="Confirm new Password">
+              </div>
+            </div>
+            <div class="mb-3 d-flex justify-content-center">
+              <input class="btn btn-primary" type="submit" value="Update password">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
   <script src="<?php echo base_url('resources/'); ?>JS/popper.min.js"></script>
   <script src="<?php echo base_url('resources/'); ?>JS/bootstrap.min.js"></script>
 </body>
