819185031c5576fdcb300ca1fba5ab8f6e34b3a8
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-10 16:07:01 -0500

    chore(Views): :lipstick: Change language to spanish

--- application/views/Login.php
+++ application/views/Login.php
@@ -15,20 +15,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <body>
 	<div class="container d-flex justify-content-center align-items-center min-vh-100">
 		<div class="w-100 p-3 border rounded" style="max-width: 500px;">
-			<h2 class="text-center mb-3">Welcome</h2>
+			<h2 class="text-center mb-3">Bienvenido</h2>
 			<form>
 				<div class="mb-3">
-					<label for="exampleFormControlInput1" class="form-label">Email address</label>
-					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+					<label for="exampleFormControlInput1" class="form-label">Correo:</label>
+					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nombre@ejemplo.com">
 				</div>
 				<div class="mb-3">
-					<label for="exampleFormControlPassword1" class="form-label">Password</label>
-					<input type="password" class="form-control" id="exampleFormControlPassword1" placeholder="Password">
+					<label for="exampleFormControlPassword1" class="form-label">Contraseña:</label>
+					<input type="password" class="form-control" id="exampleFormControlPassword1" placeholder="Tu Contraseña">
 				</div>
-				<div class="mb-3 d-flex justify-content-between">
-					<a href="register">Create Acount</a>
-					<input class="btn btn-primary" type="submit" value="Submit">
-					<a href="">Forgot Password?</a>
+				<div class="mb-3 d-flex justify-content-between align-items-center gap-3">
+					<a href="register">Crear cuenta</a>
+					<input class="btn btn-primary" type="submit" value="Iniciar sesión">
+					<a href="#">¿Olvidaste tu contraseña?</a>
 				</div>
 			</form>
 		</div>
