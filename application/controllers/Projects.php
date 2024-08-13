4dfb497c25cd695b78b5a8fdebc2644f14edc7d4
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-12 20:37:15 -0500

    chore(Controllers): :sparkles: Add helper to dashboard
    The header is added to avoid navigation through the url

--- application/controllers/Projects.php
+++ application/controllers/Projects.php
@@ -4,8 +4,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Projects extends CI_Controller
 {
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->helper('auth');
+  }
+
   public function index()
   {
+    check_login();
     $this->load->view('Projects_dashboard');
   }
 
