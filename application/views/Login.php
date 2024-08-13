2527dcfaebeb76b5b29c0b64c1105356d3f24e9a
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-08-12 21:11:51 -0500

    feat(Bussiness Logic): :sparkles: Add login
    the logic for the login is added

--- application/views/Login.php
+++ application/views/Login.php
@@ -16,21 +16,28 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<div class="container d-flex justify-content-center align-items-center min-vh-100">
 		<div class="w-100 p-3 border rounded" style="max-width: 500px;">
 			<h2 class="text-center mb-3">Bienvenido</h2>
-			<form>
-				<div class="mb-3">
-					<label for="exampleFormControlInput1" class="form-label">Correo:</label>
-					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nombre@ejemplo.com">
-				</div>
-				<div class="mb-3">
-					<label for="exampleFormControlPassword1" class="form-label">Contraseña:</label>
-					<input type="password" class="form-control" id="exampleFormControlPassword1" placeholder="Tu Contraseña">
-				</div>
-				<div class="mb-3 d-flex justify-content-between align-items-center gap-3">
-					<a href="register">Crear cuenta</a>
-					<input class="btn btn-primary" type="submit" value="Iniciar sesión">
-					<a href="#">¿Olvidaste tu contraseña?</a>
-				</div>
-			</form>
+			<?php if ($this->session->flashdata('success')): ?>
+				<p><?php echo $this->session->flashdata('success'); ?></p>
+			<?php endif; ?>
+
+			<?php if ($this->session->flashdata('error')): ?>
+				<p><?php echo $this->session->flashdata('error'); ?></p>
+			<?php endif; ?>
+			<?php echo form_open('Login'); ?>
+			<div class="mb-3">
+				<label for="user_email" class="form-label">Correo:</label>
+				<input type="email" class="form-control" id="user_email" name="user_email" placeholder="nombre@ejemplo.com">
+			</div>
+			<div class="mb-3">
+				<label for="user_password" class="form-label">Contraseña:</label>
+				<input type="password" class="form-control" id="user_password" name="user_password" placeholder="Tu Contraseña">
+			</div>
+			<div class="mb-3 d-flex justify-content-between align-items-center gap-3">
+				<a href="register">Crear cuenta</a>
+				<input class="btn btn-primary" type="submit" value="Iniciar sesión">
+				<a href="#">¿Olvidaste tu contraseña?</a>
+			</div>
+			<php echo form_close(); ?>
 		</div>
 	</div>
 	<script src="<?php echo base_url('resources/'); ?>JS/bootstrap.min.js"></script>
